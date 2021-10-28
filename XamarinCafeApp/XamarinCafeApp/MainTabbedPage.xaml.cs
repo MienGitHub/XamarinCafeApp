5f400b415713e826bcdee79c77cf949a23dcae98
MienGitHub <73860390+MienGitHub@users.noreply.github.com>
2021-10-28 17:36:21 +1300

    Add project files.

---
+++ XamarinCafeApp/XamarinCafeApp/MainTabbedPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinCafeApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainTabbedPage : TabbedPage
+    {
+        public MainTabbedPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void espressoButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EspressoPage());
+        }
+
+        private async void hotdrinksButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HotDrinksPage());
+        }
+
+        private async void colddrinksButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ColdDrinksPage());
+        }
+        private async void quickbitesButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new QuickBitesPage());
+        }
+    }
+}
